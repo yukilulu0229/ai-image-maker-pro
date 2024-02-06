a7485e915f2236e4fe98870b70e45f6c9b930d6d
Shin-sibainu <shincode0712@gmail.com>
2024-02-06 20:44:42 +0900

    ユーザーログイン時のヘッダー修正

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,6 @@
 export default function Home() {
   return (
-    <main
-      className="sm:pt-16 pt-14 min-h-screen"
-      style={{ maxHeight: "calc(100vh - 4rem)" }}
-    >
+    <main style={{ maxHeight: "calc(100vh - 4rem)" }}>
       {/* ホーム画面はMVPリリースした後に追加 */}
       <h3>SNSアイコンメーカー</h3>　{/* ./でまずはSNSアイコンメーカーを作成 */}
     </main>
